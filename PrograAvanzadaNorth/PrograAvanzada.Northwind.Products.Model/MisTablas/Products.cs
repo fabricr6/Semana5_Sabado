0057ef33a85396682dc6931aba720706863e47a1
Fabricio Lobo Guadamuz <fabricr6@hotmail.com>
2018-10-13 14:53:13 -0600

    Estructura de EF Primera Parte

---
+++ PrograAvanzadaNorth/PrograAvanzada.Northwind.Products.Model/MisTablas/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PrograAvanzada.Northwind.Products.Model
+{
+    public partial class Product
+    {
+        public Nullable<short> UnidadesDeDesabastecimiento { get
+            {
+                Nullable<short> elResultado = 0;
+                if (ReorderLevel > UnitsInStock)
+                    elResultado = (short?)(ReorderLevel - UnitsInStock);
+                    return elResultado;
+                ;
+            }
+      
+         }
+
+    }
+}
